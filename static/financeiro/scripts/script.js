4cd003d22ec1c57a3eb99cc3c792c9451ef21a3d
unknown <mths737@gmail.com>
2025-09-02 12:00:02 -0300

    ajustes

--- static/financeiro/scripts/script.js
+++ static/financeiro/scripts/script.js
@@ -21,17 +21,21 @@ function pagar(id) {
     const div = document.getElementById("pagar-"+id);
 
     div.innerHTML = `
-            <input style="width: 55%;" type="date" name="data_pagamento" required>
-            <button type="submit" class="btn btn-success btn-sm">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
-                    <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
-                </svg>
-            </button>
-            <button type="button" class="btn btn-secondary btn-sm" onclick="cancelar(${id})">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
-                </svg>
-            </button>
+            <div style="width: 100%;">
+                <input type="date" name="data_pagamento" required>
+            </div>
+            <div style="display: flex; width: 100%; justify-content: center; align-items: center;">
+                <button style="margin: 2px;" type="submit" class="btn btn-success btn-sm">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+                    </svg>
+                </button>
+                <button style="margin: 2px;" type="button" class="btn btn-secondary btn-sm" onclick="cancelar(${id})">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                    </svg>
+                </button>
+            </div>
     `;
 }
 
